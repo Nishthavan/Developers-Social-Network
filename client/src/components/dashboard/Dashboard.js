44043a7d1b16b1b3896a6cc90c6bd2aec514a222
Nishthavan dahiya <nishthavandahiya7@gmail.com>
2020-04-18 13:53:42 +0530

    Profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {getCurrentProfile} from "../../actions/profile"
 
-const Dashboard = props => {
-  return (
-        <div>
-        Dashboard
-        </div>
-      )
+const Dashboard = ({getCurrentProfile,auth,profile}) => {
+  useEffect(()=>{
+    getCurrentProfile();
+  },[]);
+
+  return <div>Dashboard</div>
 }
 
 Dashboard.propTypes = {
-
+  getCurrentProfile : PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 }
 
+const mapStateToProp = state => ({
+    auth: state.auth,
+    profile: state.profile
+})
 
-export default Dashboard;
+export default connect(mapStateToProp,{getCurrentProfile})(Dashboard);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -17,7 +17,7 @@ router.get("/me", auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({
       user: req.user.id
-    }).populate("user", ["name", "avatar"]);
+    }).populate("users", ["name", "avatar"]);
     if (!profile) {
       return res.status(400).json({
         msg: "Profile not available"
