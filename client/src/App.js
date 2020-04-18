fc4d27137da03ccbe4706e2d569ee54bb58244ab
Nishthavan dahiya <nishthavandahiya7@gmail.com>
2020-04-18 12:48:20 +0530

    PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from "./components/layout/Alert";
 import setAuthToken from "./utils/setAuthToken";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 //redux
 import {Provider} from "react-redux";
 import  store from "./store";
@@ -34,6 +36,7 @@ const App = () => {
       <Switch>
       <Route exact path = "/register" component = {Register}/>
       <Route exact path = "/login" component = {Login}/>
+      <PrivateRoute exact path = "/dashboard" component = {Dashboard}/>
       </Switch>
       </section>
     </Fragment>
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Route,
+  Redirect
+} from "react-router-dom";
+import PropTypes from "prop-types";
+import {
+  connect
+} from "react-redux";
+
+const PrivateRoute = ({
+    component: Component,
+    auth: {
+      isAuth,
+      loading
+    },
+    ...rest
+  }) => ( <
+    Route {
+      ...rest
+    }
+    render = {
+      props => !isAuth && !loading ? ( < Redirect to = "/login" / > ) : ( < Component {
+          ...props
+        }
+        />)} / >
+      );
+
+      PrivateRoute.propTypes = {
+        auth: PropTypes.object.isRequired
+      }
+      const mapStateToProp = state => ({
+        auth: state.auth
+      });
+
+      export default connect(mapStateToProp)(PrivateRoute);
